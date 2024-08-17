30442e42f67cec632a938b430aea7e0e469095aa
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-08-17 20:49:59 +0200

    add file hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import MenuItems from "./components/MenuItems"
 import { menuItems } from "./data/db"
+import useOrder from "./hooks/useOrder"
 
 function App() {
-
+  const { addItem } = useOrder();
 
   return (
     <div className="">
@@ -12,13 +13,19 @@ function App() {
 
       <main className="max-w-7xl mx-auto py-20 grid  md:grid-cols-2 ">
 
-        <div className="">
-          <h2 className="">Menu</h2>
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menú</h2>
 
-          {menuItems.map(item => (
-            <MenuItems key={item.id} menu={item} />
-          ))
-          }
+          <div className="space-y-3 mt-10">
+            {menuItems.map(item => (
+              <MenuItems
+                key={item.id}
+                item={item}
+                addItem={addItem}
+              />
+            ))
+            }
+          </div>
         </div>
 
         <div className="">
--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { MenuItems } from '../types';
 
-const MenuItems = ({ menu }) => {
-      console.log(menu);
+type MenuItemsProps = {
+      item: MenuItems,
+      addItem: () => void
+}
+
+const MenuItems = ({ item, addItem }: MenuItemsProps) => {
+      const { id, name, price } = item;
+
       return (
-            <div key={menu.id}>MenuItems</div>
+            <button
+                  className='border border-teal-400 w-full flex items-center justify-between p-3 hover:bg-teal-200'
+                  onClick={() => addItem()}
+            >
+                  <p className="">{name}</p>
+                  <p className="font-black">$ {price}</p>
+            </button>
       )
 }
 

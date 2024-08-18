f2eec41c3176aee1d2c968f50f1dd4f543c49899
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-08-18 19:57:39 +0200

    calculating the total to pay

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -10,6 +10,8 @@ type OrderTotalProps = {
 const OrderTotals = ({ order, tip }: OrderTotalProps) => {
       const subtotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order]);
       const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order]);
+      const totalAmount = useMemo(() => subtotalAmount + tipAmount, [tip, order]);
+
 
       return (
             <>
@@ -24,7 +26,7 @@ const OrderTotals = ({ order, tip }: OrderTotalProps) => {
                         </p>
 
                         <p>Total a pagar: {''}
-                              <span className='font-bold'>$0</span>
+                              <span className='font-bold'>{formatCurrency(totalAmount)}</span>
                         </p>
                   </div>
 
